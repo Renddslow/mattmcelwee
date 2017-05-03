40ac678866992413a98d18615f3fcbca66e9f713
Matt McElwee <mubatt@wyopub.com>
2017-05-02 21:56:08 -0500

    Added resume json

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import json
+
 from flask import Flask
 
 application = Flask(__name__)
 
+
 @application.route("/resume")
+def resume():
+	data = open('resume.json')
+	resume = json.load(data)
+	return render_template("resume.html", resume=resume)
+
+
+if __name__ == "__main__":
+	application.run(host='0.0.0.0')
